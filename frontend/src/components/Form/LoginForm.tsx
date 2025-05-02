da13571b6ebe5340341eeaa5909e700bb4f64fae
MeowB <meolearn@gmail.com>
2025-05-02 15:57:38 +0200

    login linked to backend

--- frontend/src/components/Form/LoginForm.tsx
+++ frontend/src/components/Form/LoginForm.tsx
@@ -2,24 +2,28 @@ import axios from "axios"
 import { FormEvent, useState } from "react"
 
 const LoginForm = () => {
-	const [formData, setFormData] = useState({
-		email: '',
-		password: ''
-	})
 
 	const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 
+		const form = e.currentTarget
+		const formData = new FormData(form)
+
+		const email = formData.get('email') as string
+		const password = formData.get('password') as string
+
 		try {
+			const res = await axios.post('/api/login', { email, password })
+			console.log(res.data)
 
 		} catch (error) {
-			
+			console.error('Error loging in: ', error)
 		}
 	}
 
 	return (
 		<div>
-			<form action="POST">
+			<form action="POST" onSubmit={handleLogin}>
 				<div className="form-group">
 					<input type="email" id="email" name="email" placeholder="Email" required />
 				</div>
